dbf92e5f78caf14b650c8b2df946333e64239514
LazycoderPHD <phatdao2309@gmail.com>
2026-02-28 12:58:24 +0700

    ANTI LEFT JOIN CUSTOMERS, ORDERS DONE!

--- sql1git.sql
+++ sql1git.sql
@@ -19,3 +19,19 @@ select
 	score
 from sales.Customers
 where score is null
+
+--list all details for customers who have not placed any orders
+
+select
+	c.*,
+	o.OrderID
+	--c.CustomerID,
+	--c.Firstname + ' ' + coalesce(c.Lastname, '') as [Customer's FullName]
+from sales.Customers as c
+left join sales.Orders as o
+on c.CustomerID = o.CustomerID
+where o.customerID is null
+
+select
+	*
+from sales.Orders
