d5dce9ff7cb1b26224eba7a9861cc4bccf9de26c
LazycoderPHD <phatdao2309@gmail.com>
2026-04-15 06:19:10 +0700

    Homework assignment done

--- sql4git.sql
+++ sql4git.sql
@@ -1,1 +1,17 @@
-hi
+--find the total sales across all orders
+select sum (sales) as [total sales] from sales.Orders
+
+--find the total sales for each product
+select productID, sum (sales) as [total sales] from sales.Orders
+group by productID
+
+--Find the total sales for each product, additionally provide details such order id & order date
+select OrderDate,productID, orderID, sum (sales) as [total sales] from sales.Orders
+group by productID, orderID, OrderDate
+
+select sum (sales) over() as [total_sales] from sales.Orders
+select productID, sum (sales) over(partition by productID) as [total_sales] from sales.Orders --dont need productID listed in partition by unlike group by
+select OrderDate,productID, orderID, sum (sales) over(partition by productID) as [total_sales] from sales.Orders
+select OrderDate,productID, orderID, sum (sales) over(partition by orderID) as [total_sales] from sales.Orders
+
+
