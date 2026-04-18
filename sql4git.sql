90f494c93061bf24c7554920b5b2459ce7b57231
LazycoderPHD <phatdao2309@gmail.com>
2026-04-19 01:02:09 +0700

    WIP, busy eating

--- sql4git.sql
+++ sql4git.sql
@@ -42,6 +42,7 @@ from sales.Orders
 --============WINDOW FUNCTION: ORDER BY=================
 -- Rank each order based on their sales from highest to lowest
 -- Additionally provide details such order Id, order date
+--ORDER BY always uses a frame. Default is "rows between unbounded preceding and current row"
 
 select orderdate, sales,
 rank() over(order by sales desc) Ranksales
@@ -63,3 +64,14 @@ select orderid, orderdate, orderstatus, sales,
 sum(sales) over(partition by orderstatus order by orderdate
 rows between 2 preceding and current row) [totalsales row - 2, current]
 from sales.Orders
+
+--Rank customers based on their total sales
+--select coalesce(firstname, '') + ' ' + coalesce(lastname, '') [full name] from sales.Customers c
+--join sales.Orders o on c.CustomerID = o.CustomerID
+
+select * from sales.Orders
+
+select o.customerID, coalesce(firstname, '') + ' ' + coalesce(lastname, '') [fullname], sum(sales) [TotalSales], rank() over(order by sum(sales) desc) RankCustomers
+from sales.Orders o
+join sales.Customers c on c.CustomerID = o.CustomerID 
+group by o.customerID, coalesce(firstname, '') + ' ' + coalesce(lastname, '')
